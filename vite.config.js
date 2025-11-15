879ad07e1a2d3bd0c373f00c4ede4a7fd07fe56e
Mustapha Abdullahi <mustypro137@gmail.com>
2025-11-15 12:48:11 +0100

    redeploy

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/simple-feature-section-master/',
-  build: {
-    outDir: 'docs',
-  },
+  base: '/simple-feature-section-master',
 });
